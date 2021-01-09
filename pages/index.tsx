84ad4c3ac95800f47943f9db026e892c36f993d9
rokumura7 <rokumura.8807@gmail.com>
2021-01-09 18:44:21 +0900

    removed Nav.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
-import Nav from '../components/Nav'
-
 const Index = () => (
   <div>
-    <Nav />
     <div className="py-20">
       <h1 className="text-5xl text-center text-accent-1">
         Next.js + Tailwind CSS
