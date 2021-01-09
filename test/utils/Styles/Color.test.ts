8d1e4fc58941b23906f98e99ee7b616d197dc625
rokumura7 <rokumura.8807@gmail.com>
2021-01-09 18:40:37 +0900

    fixed Color.test.ts

--- test/utils/Styles/Color.test.ts
+++ test/utils/Styles/Color.test.ts
@@ -1,7 +1,16 @@
 import { getColorSet } from '@/utils/Styles/Color'
 
 describe('Test for Utils.replaceIndex()', () => {
-  it('should return correct class names.', () => {
+  it('should return correct class names without args.', () => {
+    const actual = getColorSet()
+    const expected = {
+      bg: 'bg-gray-500',
+      bgHover: 'hover:bg-gray-600',
+      text: 'text-white',
+    }
+    expect(actual).toStrictEqual(expected)
+  })
+  it('should return correct class names with args.', () => {
     const actual = getColorSet('default')
     const expected = {
       bg: 'bg-gray-500',
