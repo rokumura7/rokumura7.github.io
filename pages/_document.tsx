e3347cce4d0a4deba93b9b2d5483e9e80377b3b8
rokumura7 <rokumura.8807@gmail.com>
2021-01-09 22:18:26 +0900

    added favicon

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>rokumura | portfolio</title>
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
