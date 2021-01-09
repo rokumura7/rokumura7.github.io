fb15d7ad200d85ecf38e9f592c750018c4c090d7
rokumura7 <rokumura.8807@gmail.com>
2021-01-09 14:47:08 +0900

    import tailwindcss and next

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Nav from '../components/Nav'
+
+const Index = () => (
+  <div>
+    <Nav />
+    <div className="py-20">
+      <h1 className="text-5xl text-center text-accent-1">
+        Next.js + Tailwind CSS
+      </h1>
+    </div>
+  </div>
+)
+
+export default Index
--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom'
-
-const HelloWorld = () => <div>HelloWorld</div>
-
-ReactDOM.render(<HelloWorld />, document.getElementById('root'))
