f59915247097b13042652c35c79bc71bba8a28c0
rokumura7 <rokumura.8807@gmail.com>
2021-01-16 12:20:15 +0900

    fix AboutPage's design

--- components/containers/pages/AboutPage.tsx
+++ components/containers/pages/AboutPage.tsx
@@ -3,14 +3,18 @@ import GitHubGrassGraph from '../../components/atoms/GitHubGrassGraph'
 
 const AboutPage = () => (
   <Layout>
-    <div className="py-20">
-      <h1 className="text-5xl text-center text-accent-1">This is AboutPage</h1>
-      <div className="flex">
-        <div className="w-1/2">
-          <p>ここに自己紹介</p>
-        </div>
-        <div className="w-1/2">
-          <GitHubGrassGraph />
+    <div className="pt-20 bg-black">
+      <div className="h-3/5"></div>
+      <div className="bg-white h-2/5 leaning"></div>
+      <div className="absolute pt-20 inset-0 h-full">
+        <div className="h-full">
+          <div className="h-3/5 text-white">
+            <h1 className="text-5xl text-center">Reo Okumura</h1>
+            <p>I’m a Full Stack Web Developer who likes Static Typing.</p>
+          </div>
+          <div className="h-2/5 p-2">
+            <GitHubGrassGraph />
+          </div>
         </div>
       </div>
     </div>
