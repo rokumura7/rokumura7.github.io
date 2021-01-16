4455c1cf6ee1112f337030306a94331400cc17cc
rokumura7 <rokumura.8807@gmail.com>
2021-01-16 11:32:26 +0900

    wip works

--- components/containers/pages/WorksPage.tsx
+++ components/containers/pages/WorksPage.tsx
@@ -5,6 +5,7 @@ const AboutPage = () => (
   <Layout>
     <div className="py-20">
       Coming soon...
+      {/* <Link href="https://scratch.mit.edu/projects/200882990/">さめ</Link> */}
       {/* <Link href="/works/tictactoe">TicTacToe</Link> */}
     </div>
   </Layout>
