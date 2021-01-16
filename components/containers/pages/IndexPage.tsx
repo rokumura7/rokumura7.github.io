8b381a4113d67ce146e405ca68b4adc570739cbc
rokumura7 <rokumura.8807@gmail.com>
2021-01-16 14:41:47 +0900

    fix video position

--- components/containers/pages/IndexPage.tsx
+++ components/containers/pages/IndexPage.tsx
@@ -3,7 +3,7 @@ import Layout from '../layouts/Layout'
 const IndexPage = () => (
   <Layout>
     <div className="bg-black z-40">
-      <video className="z-0 min-h-full" autoPlay loop muted>
+      <video className="z-0 min-h-full m-auto" autoPlay loop muted>
         <source src="/static/mov/jellyfish.mp4" type="video/mp4"></source>
       </video>
       <div className="flex justify-center items-center absolute inset-0 text-white">
