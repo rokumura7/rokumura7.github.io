d5016fb4629fa137686114c2c87189ba2151a88c
rokumura7 <rokumura.8807@gmail.com>
2021-01-23 19:40:04 +0900

    fix github grass graph

--- components/components/atoms/GitHubGrassGraph.stories.tsx
+++ components/components/atoms/GitHubGrassGraph.stories.tsx
@@ -1,11 +1,13 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import GitHubGrassGraph from './GitHubGrassGraph'
+import GitHubGrassGraph, { GitHubGrassGraphProps } from './GitHubGrassGraph'
 
 export default {
   title: 'Example/GitHubGrassGraph',
   component: GitHubGrassGraph,
 } as Meta
 
-const Template: Story = () => <GitHubGrassGraph />
+const Template: Story<GitHubGrassGraphProps> = (
+  props: GitHubGrassGraphProps,
+) => <GitHubGrassGraph {...props} />
 
 export const GitHubGrassGraphExample = Template.bind({})
--- components/components/atoms/GitHubGrassGraph.tsx
+++ components/components/atoms/GitHubGrassGraph.tsx
@@ -1,6 +1,16 @@
-const GitHubGrassGraph = () => (
+export interface GitHubGrassGraphProps {
+  rotate?: boolean
+}
+
+const getUrl = (rotate?: boolean) => {
+  const url =
+    'https://grass-graph.moshimo.works/images/rokumura7.png?background=none'
+  return rotate ? url + '&rotate=90' : url
+}
+
+const GitHubGrassGraph = (props: GitHubGrassGraphProps) => (
   <a href="https://github.com/rokumura7">
-    <img src="https://grass-graph.moshimo.works/images/rokumura7.png?background=none"></img>
+    <img src={getUrl(props.rotate)}></img>
   </a>
 )
 
