d587e81f075da514117a30aada22faa85e126e28
rokumura7 <rokumura.8807@gmail.com>
2021-02-11 17:55:02 +0900

    deleted GitHubGrassGraph

--- components/components/atoms/index.ts
+++ components/components/atoms/index.ts
@@ -1,9 +1,8 @@
 import Bubble from './Bubble'
 import Button from './Button'
-import GitHubGrassGraph from './GitHubGrassGraph'
 import Icon from './Icon'
 import Image from './Image'
 import MyDevIcon from './MyDevIcon'
 import MyLink from './MyLink'
 
-export { Bubble, Button, GitHubGrassGraph, Icon, Image, MyDevIcon, MyLink }
+export { Bubble, Button, Icon, Image, MyDevIcon, MyLink }
